24f7f08cc0eaaffad4ad9de221aa117da29ce2cd
AndyLin <croc0909@gmail.com>
2022-09-03 10:00:45 +0800

    Initial Commit

---
+++ SelfIntroduction/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SelfIntroduction
+//
+//  Created by AndyLin on 2022/9/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
